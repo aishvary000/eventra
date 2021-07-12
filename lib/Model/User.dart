dd553461b2b0c32c677c97c58bea228a51b4d76b
aishvary000 <54467728+aishvary000@users.noreply.github.com>
2021-07-12 22:36:44 +0530

    Updated Bottom navigation Bar

--- lib/Model/User.dart
+++ lib/Model/User.dart
@@ -2,6 +2,7 @@ class user{
   String name;
   String email;
   String designation;
+  List<String> clubName;
   String uid;
-  user({required this.uid,required this.email,required this.designation,required this.name});
+  user({required this.uid,required this.email,required this.designation,required this.name,required this.clubName});
 }
